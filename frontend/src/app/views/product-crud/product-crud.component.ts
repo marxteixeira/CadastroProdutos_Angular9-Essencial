b610adfd39e9993ea4a9916baea470468294317d
marxtts@gmail.com <95075808+marxteixeira@users.noreply.github.com>
2023-07-21 07:49:30 -0300

    inserindo interface para produtos

--- frontend/src/app/views/product-crud/product-crud.component.ts
+++ frontend/src/app/views/product-crud/product-crud.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-crud',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class ProductCrudComponent {
 
+  constructor(private router: Router) {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  navigateToProductCreate(): void {
+    this.router.navigate(['/products/create'])
+  }
 }
