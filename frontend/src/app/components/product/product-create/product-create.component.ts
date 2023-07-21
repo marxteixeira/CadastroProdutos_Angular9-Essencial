619adf740cde33469522c372ea636e70d471a802
marxtts@gmail.com <95075808+marxteixeira@users.noreply.github.com>
2023-07-21 18:15:08 -0300

    adicionando service

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,4 +1,6 @@
+import { ProductService } from './../product.service';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-create',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-create.component.css']
 })
 export class ProductCreateComponent {
+
+  constructor(private productService: ProductService,
+    private router: Router) {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  createProduct(): void {
+    this.productService.showMessage('Produto criado!')
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
+  }
 
 }
